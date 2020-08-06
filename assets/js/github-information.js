0b15a8d880ac35226e577f5191877b0d75bdde7e
jakefernihough <jakefernihough@gmail.com>
2020-08-06 10:00:24 +0000

    Fix bugs fot he gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -17,18 +17,18 @@ function userInformationHTML(user) {
 
 function repoInformationHTML(repos) {
     if (repos.length == 0) {
-        return `<div class="clearfix repo-list">No repos!</div>`;  
+        return `<div class="clearfix repo-list">No repos!</div>`;
     }
 
     var listItemsHTML = repos.map(function(repo) {
         return `<li>
-                      <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-                </li>`
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
     });
 
     return `<div class="clearfix repo-list">
                 <p>
-                     <strong>Repo List</strong>
+                    <strong>Repo List:</strong>
                 </p>
                 <ul>
                     ${listItemsHTML.join("\n")}
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -70,3 +72,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
